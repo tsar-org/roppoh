426fbfab50fa2abeaa214dc90398bc4ae1973575
taiki-kuraishi <kuraishi.taiki0@gmail.com>
2025-01-31 12:46:21 +0900

    Rename next.config.ts -> next.config.mjs for run next dev

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,8 @@ if (process.env.NODE_ENV === 'development') {
   await setupDevPlatform();
 }
 
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
 };
 
