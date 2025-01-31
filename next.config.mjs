3d667ff34f3023133289586569e5ca03d790f7c0
Kuraishi Taiki <131664944+taiki-kuraishi@users.noreply.github.com>
2025-01-31 18:36:11 +0900

    Setup Environment (#1)
    * Run 'pnpm create cloudflare@latest my-next-app --framework=next'
    
    * setup pnpm engins settings
    
    * Add .editorconfig
    
    * Add 'NODE_VERSION' to env var
    
    * Rename next.config.ts -> next.config.mjs for run next dev
    
    * Setup Eslint config
    
    * Setup prettier
    
    * Setup Lefthook
    
    * Add '@next/bundle-analyzer'
    
    * Add dependabot.yml
    
    * Add github actions workflow
    
    * Update Readme.md
    
    * Integrate analyze-and-comment-bundle-sizes.yml into ci.yml

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+// Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
+// (when running the application with `next dev`), for more information see:
+// https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+};
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+export default bundleAnalyzer(nextConfig);
