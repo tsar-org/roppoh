5a170e35e2bdc6e7364edf60a6a97095892993f8
taiki-kuraishi <kuraishi.taiki0@gmail.com>
2025-02-02 19:04:40 +0900

    Enhance user experience by adding animations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
+import { AnimatePresence } from "framer-motion";
 
 export default function RootLayout({
   children,
@@ -9,14 +10,16 @@ export default function RootLayout({
   return (
     <html lang="ja" suppressHydrationWarning>
       <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <AnimatePresence mode="wait">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </AnimatePresence>
       </body>
     </html>
   );
--- src/app/unity-sports-resort/page.tsx
+++ src/app/unity-sports-resort/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
-import { UnitySportsResort } from "@/app/unity-sports-resort/_components/unitySportsResort";
+import Loading from "@/components/Loading";
+import PageTransition from "@/components/pageTransition";
 import { useDiscordSDK } from "@/hooks/useDiscordSDK";
-import { Suspense, useEffect } from "react";
+import { Suspense, lazy, useEffect } from "react";
+
+const UnitySportsResort = lazy(
+  () => import("@/app/unity-sports-resort/_components/unitySportsResort"),
+);
 
 export default function UnitySportsResortPage() {
   const discordId = process.env.DISCORD_ID as string;
@@ -14,10 +19,16 @@ export default function UnitySportsResortPage() {
   }, [setupDiscordSDK]);
 
   return (
-    <>
-      <Suspense fallback={<div>Loading...</div>}>
+    <PageTransition>
+      <Suspense
+        fallback={
+          <div className="h-[100vh] w-[100vw]">
+            <Loading />
+          </div>
+        }
+      >
         <UnitySportsResort />
       </Suspense>
-    </>
+    </PageTransition>
   );
 }
