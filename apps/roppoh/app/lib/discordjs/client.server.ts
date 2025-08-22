e7a85e82bab07e103a9ab55dcb4b609693240f06
taiki-kuraishi <taiki-kuraishi@users.noreply.github.com>
2025-08-22 18:16:01 +0900

    fix

--- apps/roppoh/app/lib/discordjs/client.server.ts
+++ apps/roppoh/app/lib/discordjs/client.server.ts
@@ -1,13 +1,11 @@
 import { API } from "@discordjs/core/http-only";
 import { REST } from "@discordjs/rest";
 import type { RESTOptions, ResponseLike } from "@discordjs/rest";
-import type { RequestInit } from "undici";
 
 const restOption: Partial<RESTOptions> = {
   authPrefix: "Bearer",
-  makeRequest: (url: string, init: RequestInit) => {
-    return globalThis.fetch(url, init as globalThis.RequestInit) as Promise<ResponseLike>;
-  },
+  makeRequest: (url, init) =>
+    globalThis.fetch(url, init as globalThis.RequestInit) as Promise<ResponseLike>,
 };
 
 export const createDiscordApiClient = ({ token }: { token: string }) => {
