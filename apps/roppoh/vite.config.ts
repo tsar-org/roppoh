14d7876c8913a09227c9350de501550e12c3d8c3
taiki-kuraishi <taiki-kuraishi@users.noreply.github.com>
2025-08-26 18:50:52 +0900

    feat: pwa

--- apps/roppoh/vite.config.ts
+++ apps/roppoh/vite.config.ts
@@ -2,6 +2,7 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
@@ -9,6 +10,25 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      manifest: {
+        name: "roppoh",
+        icons: [
+          {
+            sizes: "192x192",
+            src: "tsar-icon.png",
+            type: "image/png",
+          },
+          {
+            sizes: "512x512",
+            src: "tsar-icon.png",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
     cloudflare({
       viteEnvironment: { name: "ssr" },
       configPath: "./wrangler.jsonc",
