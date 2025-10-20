4e885655e812529562e10d5f7d113be7b85e929c
Kuraishi Taiki <kuraishi.taiki0@gmail.com>
2025-10-20 15:41:43 +0900

    ✨ Add 404-not-found page (#183)
    * add 404 not found page
    
    * fix: ci
    
    ---------
    
    Co-authored-by: taiki-kuraishi <taiki-kuraishi@users.noreply.github.com>

---
+++ apps/roppoh/test/visual-regression/pages/404.spec.tsx
@@ -0,0 +1,84 @@
+import { createRoutesStub } from "react-router";
+import { Theme } from "remix-themes";
+import { describe, expect, test } from "vitest";
+import { page } from "vitest/browser";
+import { render } from "vitest-browser-react";
+import NotFoundPage from "@/pages/404/page";
+import Root, { type loader } from "@/root";
+import { setDesktopViewPort, setMobileViewPort } from "../helpers/view-port";
+
+describe("VRT 404-not-found page", async () => {
+  const PATH = "/404";
+  const routeChildren = [{ Component: NotFoundPage, path: PATH }];
+
+  test("desktop dark", async () => {
+    // Arrange
+    await setDesktopViewPort(page);
+    const Stub = createRoutesStub([
+      {
+        Component: Root,
+        children: routeChildren,
+        loader: async (): ReturnType<typeof loader> => ({ theme: Theme.DARK }),
+      },
+    ]);
+
+    // Act
+    const screen = render(<Stub initialEntries={[PATH]} />);
+
+    // Assert
+    await expect(screen.container).toMatchScreenshot("desktop-dark");
+  });
+
+  test("desktop light", async () => {
+    // Arrange
+    await setDesktopViewPort(page);
+    const Stub = createRoutesStub([
+      {
+        Component: Root,
+        children: routeChildren,
+        loader: async (): ReturnType<typeof loader> => ({ theme: Theme.LIGHT }),
+      },
+    ]);
+
+    // Act
+    const screen = render(<Stub initialEntries={[PATH]} />);
+
+    // Assert
+    await expect(screen.container).toMatchScreenshot("desktop-light");
+  });
+
+  test("mobile dark", async () => {
+    // Arrange
+    await setMobileViewPort(page);
+    const Stub = createRoutesStub([
+      {
+        Component: Root,
+        children: routeChildren,
+        loader: async (): ReturnType<typeof loader> => ({ theme: Theme.DARK }),
+      },
+    ]);
+
+    // Act
+    const screen = render(<Stub initialEntries={[PATH]} />);
+
+    // Assert
+    await expect(screen.container).toMatchScreenshot("mobile-dark");
+  });
+
+  test("mobile light", async () => {
+    await setMobileViewPort(page);
+    const Stub = createRoutesStub([
+      {
+        Component: Root,
+        children: routeChildren,
+        loader: async (): ReturnType<typeof loader> => ({ theme: Theme.LIGHT }),
+      },
+    ]);
+
+    // Act
+    const screen = render(<Stub initialEntries={[PATH]} />);
+
+    // Assert
+    await expect(screen.container).toMatchScreenshot("mobile-light");
+  });
+});
