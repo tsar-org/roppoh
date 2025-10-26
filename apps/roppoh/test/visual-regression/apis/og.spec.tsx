c9f4282bcdaf481f53d9ea23424f2e68eba44552
taiki-kuraishi <taiki-kuraishi@users.noreply.github.com>
2025-10-26 20:36:07 +0900

    add vrt test

---
+++ apps/roppoh/test/visual-regression/apis/og.spec.tsx
@@ -0,0 +1,102 @@
+import { describe, expect, test } from "vitest";
+import { type BrowserPage, page } from "vitest/browser";
+import { render } from "vitest-browser-react";
+import { OG } from "@/apis/og/components/og";
+
+const seOgViewPort = (page: BrowserPage) => page.viewport(1200, 630);
+
+function OgRoot({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      style={{
+        height: 630,
+        width: 1200,
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+describe("VRT OG", async () => {
+  test("empty title", async () => {
+    // Arrange
+    await seOgViewPort(page);
+    const title = "";
+
+    // Act
+    const screen = render(
+      <OgRoot>
+        <OG title={title} />
+      </OgRoot>,
+    );
+
+    // Assert
+    await expect(screen.container).toMatchScreenshot();
+  });
+
+  test("short title", async () => {
+    // Arrange
+    await seOgViewPort(page);
+    const title = "Welcome";
+
+    // Act
+    const screen = render(
+      <OgRoot>
+        <OG title={title} />
+      </OgRoot>,
+    );
+
+    // Assert
+    await expect(screen.container).toMatchScreenshot();
+  });
+
+  test("medium title", async () => {
+    // Arrange
+    await seOgViewPort(page);
+    const title = "Welcome to Roppoh";
+
+    // Act
+    const screen = render(
+      <OgRoot>
+        <OG title={title} />
+      </OgRoot>,
+    );
+
+    // Assert
+    await expect(screen.container).toMatchScreenshot();
+  });
+
+  test("long title", async () => {
+    // Arrange
+    await seOgViewPort(page);
+    const title =
+      "This is a very long title to verify text wrapping and truncation behavior";
+
+    // Act
+    const screen = render(
+      <OgRoot>
+        <OG title={title} />
+      </OgRoot>,
+    );
+
+    // Assert
+    await expect(screen.container).toMatchScreenshot();
+  });
+
+  test("multiline title", async () => {
+    // Arrange
+    await seOgViewPort(page);
+    const title = "Welcome to Roppoh\nmulti line text. AAAAAAAAAA";
+
+    // Act
+    const screen = render(
+      <OgRoot>
+        <OG title={title} />
+      </OgRoot>,
+    );
+
+    // Assert
+    await expect(screen.container).toMatchScreenshot();
+  });
+});
