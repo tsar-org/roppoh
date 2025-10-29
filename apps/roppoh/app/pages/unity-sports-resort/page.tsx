02435b67ab5ed48b5bb1718252110aa4b4673ac3
Kuraishi Taiki <kuraishi.taiki0@gmail.com>
2025-10-30 01:01:20 +0900

    ✨ Add WebGL loading progress bar for Unity application (#193)
    Co-authored-by: taiki-kuraishi <taiki-kuraishi@users.noreply.github.com>

--- apps/roppoh/app/pages/unity-sports-resort/page.tsx
+++ apps/roppoh/app/pages/unity-sports-resort/page.tsx
@@ -1,8 +1,9 @@
+import { Unity, useUnityContext } from "react-unity-webgl";
 import PageTransition from "@/components/page-transition";
+import { Progress } from "@/shadcn/components/ui/progress";
 import { generateBaseMeta } from "@/utils/base-meta-function";
 import { themeSessionResolver } from "@/utils/sessions.server";
 import type { Route } from "./+types/page";
-import UnitySportsResort from "./components/unity-sports-resort";
 
 export const meta = ({ loaderData }: Route.MetaArgs) => [
   ...generateBaseMeta({
@@ -21,9 +22,33 @@ export async function loader({ request, context }: Route.LoaderArgs) {
 }
 
 export default function () {
+  const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
+    codeUrl:
+      "https://roppoh-assets.tsar-bmb.org/unity-sports-resort.wasm.unityweb",
+    dataUrl:
+      "https://roppoh-assets.tsar-bmb.org/unity-sports-resort.data.unityweb",
+    frameworkUrl:
+      "https://roppoh-assets.tsar-bmb.org/unity-sports-resort.framework.js.unityweb",
+    loaderUrl:
+      "https://roppoh-assets.tsar-bmb.org/unity-sports-resort.loader.js",
+  });
+
   return (
     <PageTransition>
-      <UnitySportsResort />
+      {!isLoaded && (
+        <Progress
+          className="absolute top-0 right-0 bottom-0 left-0 m-auto w-[60%]"
+          value={Math.round(loadingProgression * 100)}
+        />
+      )}
+      <Unity
+        style={{
+          height: "100vh",
+          visibility: isLoaded ? "visible" : "hidden",
+          width: "100vw",
+        }}
+        unityProvider={unityProvider}
+      />
     </PageTransition>
   );
 }
