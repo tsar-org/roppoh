3b6d3b3174cb34597802b104f2e89ac87b77dc93
Kuraishi Taiki <kuraishi.taiki0@gmail.com>
2026-04-18 14:34:27 +0900

    ✨ Add token_endpoint_auth_method select to OIDC client dialogs (ura-roppoh) (#265)

--- apps/ura-roppoh/src/pages/oidc-client/components/update-dialog/components/form-field.ts
+++ apps/ura-roppoh/src/pages/oidc-client/components/update-dialog/components/form-field.ts
@@ -1,9 +1,16 @@
 import * as v from "valibot";
 
+export const TOKEN_ENDPOINT_AUTH_METHODS = [
+  "none",
+  "client_secret_basic",
+  "client_secret_post",
+] as const;
+
 export const schema = v.object({
   client_name: v.pipe(v.string(), v.minLength(3, "Client name must be at least 3 characters")),
   redirect_uris: v.pipe(
     v.array(v.pipe(v.string(), v.url("Must be a valid URL"))),
     v.minLength(1, "At least one redirect URI is required"),
   ),
+  token_endpoint_auth_method: v.picklist(TOKEN_ENDPOINT_AUTH_METHODS),
 });
